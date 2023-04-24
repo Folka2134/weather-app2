05606123599aecb90eaa69768f8a85a779a7c8fe
Folka2134 <folka2134@gmail.com>
2023-04-24 18:34:29 +0100

    grabbing weather properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import "./App.css";
 import notfound from "./assets/images/404.png";
 
 const App = () => {
-  const [city, setCity] = useState("Brighton");
+  const [city, setCity] = useState(null);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
 
   const APIKey = "728b0ee6df5687559812bd3169ad77b7";
 
-  useEffect(() => {
-    getWeather();
-  }, []);
-
-  async function getWeather() {
-    await axios(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=f6b39355d32df8d5b4bb9916251c1611`
-    )
-      .then((response) => {
-        setData(response.data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }
   function handleChange(event) {
     setCity(event.target.value);
   }
 
   const handleKeypress = (e) => {
-    // it triggers by pressing the enter key
     if (e.keyCode === 13) {
       getWeather();
     }
   };
 
+  async function getWeather() {
+    const result = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=f6b39355d32df8d5b4bb9916251c1611`
+    );
+    result.json().then((json) => {
+      console.log(json);
+      if (json.cod === "404") {
+        console.log(json.message);
+      } else {
+        setDescription(json.weather.description);
+        setTemperature(json.main.temp);
+        setHumidity(json.main.humidity);
+        setWind(json.wind.speed);
+      }
+    });
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <div className="bg-gray-400 min-h-screen flex flex-col justify-center items-center">
-      <div className="relative w-[400px] h-[105px] bg-white p-4 overflow-hidden rounded-xl ease-out duration-75">
-        <div className="search-box flex items-center justify-between w-full h-min bg-white p-4 rounded-lg">
+      <div className="container relative w-[400px] h-[105px] bg-white p-4 overflow-hidden rounded-xl ease-out duration-75">
+        <div className="search-box flex items-center justify-between w-full h-min p-5 bg-white rounded-lg">
           <i className="fa-solid fa-location-dot "></i>
           <input
-            className="bg-gray-200 w-[80%] text-lg font-semibold uppercase pl-8 rounded-full"
+            className="bg-gray-200 w-[80%] text-lg font-semibold capitalize pl-8 rounded-full"
             type="text"
             placeholder="Enter Your Location"
             onChange={handleChange}
@@ -58,8 +64,9 @@ const App = () => {
           ></button>
         </div>
 
-        <div className="not-found">
-          <img src={notfound} alt="404" />
+        <div className="hidden not-found w-full text-center mt-12 scale-0 opacity-0">
+          <img className="w-[70%]" src={notfound} alt="404" />
+          <p className="text-[#06283D] text-sm font-semibold mt-3"></p>
         </div>
 
         <div className="weather-box text-center">
@@ -70,18 +77,20 @@ const App = () => {
           <p className="description text-[#06283D] text-lg font-semibold capitalize"></p>
         </div>
 
-        <div className="weather-details flex w-full justify-between mt-8  ">
+        <div className="weather-details flex w-full h-[100px] items-center ">
           <div className="humidity flex items-center w-[50%] h-[100px] p-6 justify-start">
             <i className="fa-solid fa-water px-2"></i>
             <div>
-              <span className="text-[#06283D] text-lg font"></span>
+              <span className="text-[#06283D] text-lg font">
+                {data && data.main.humidity}
+              </span>
               <p>Humidity</p>
             </div>
           </div>
           <div className="wind flex items-center w-[50%] h-[100px] p-6 justify-end">
             <i className="fa-solid fa-wind px-2"></i>
             <div>
-              <span></span>
+              <span>{data && data.wind.speed}</span>
               <p>Wind Speed</p>
             </div>
           </div>
